2d50c36b665f5def3f17eceb321344e197c314e8
hypercube21 <hypercube21@gmail.com>
2014-09-24 12:48:39 +0000

    Store is now populated from the database!

--- webapp/app/store/BooksStore.js
+++ webapp/app/store/BooksStore.js
@@ -1,4 +1,4 @@
-var itemsPerPage = 1;
+var itemsPerPage = 10;
 Ext.define('BM.store.BooksStore', {
     extend: 'Ext.data.Store',
     model: 'BM.model.BooksModel',
@@ -9,15 +9,14 @@ Ext.define('BM.store.BooksStore', {
         }
     },
     autoSync: true, //this can cause performance issues!
-    pageSize: 1,
+    pageSize: 10,
 
     //    groupField: 'author',
 
     proxy: {
         type: 'ajax',
         api: {
-            read: 'data/books.json',
-            update: 'data/updateBooks.json'
+        	read: 'books?event=get-books'
         },
         reader: {
             type: 'json',
