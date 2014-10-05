2bc6a8cf5c224efe09c10d3ed8c591c42c896660
hypercube21@gmail.com <hypercube21@gmail.com@0d024af5-6d5a-f534-3c1a-20f54fc11ff1>
2014-10-05 09:58:52 +0000

    Added front and back cover functionalities. Still needs to be dinamically uploaded.

--- trunk/webapp/app/view/book/BookWindow.js
+++ trunk/webapp/app/view/book/BookWindow.js
@@ -8,16 +8,20 @@ Ext.define('BM.view.book.BookWindow', {
                 'BM.view.categorie.CategorieCombo'
             ],
             minHeight: 300,
-            minWidth: 300,
+            minWidth: 400,
             layout: {
-            	type: 'vbox',
-            	align:'stretch'
-            },            
+                type: 'vbox',
+                align: 'stretch'
+            },
             items: [
                 {
                     xtype: 'form',
                     itemId: 'bookform',
                     bodyPadding: 10,
+                    fieldDefaults: {
+                        labelAlign: 'left',
+                        labelWidth: 90
+                    },
                     items: [
                         {
                             xtype: 'hidden',
@@ -83,6 +87,76 @@ Ext.define('BM.view.book.BookWindow', {
                             xtype: 'checkbox',
                             fieldLabel: 'Citita',
                             name: 'citita'
+                        },
+                        {
+                            xtype: 'panel',
+                            hideLabel: true,
+                            layout: 'hbox',
+                            height: 100,
+                            border: false,
+                            items: [
+                                {
+                                    xtype: 'label',
+                                    text: 'Front cover:',
+                                    width: 90
+                                },
+                                {
+                                    xtype: 'component',
+                                    height: 100,
+                                    width: 100,
+                                    autoEl: {
+                                        tag: 'img',
+                                        src: 'https://d.gr-assets.com/books/1353277730m/11588.jpg',
+                                        itemId: 'frontCoverPreview'
+                                    }
+                                },
+                                {
+                                    xtype: 'filefield',
+                                    buttonOnly: false,
+                                    buttonText: "Upload",
+                                    name: 'frontCoverUpload',
+                                    buttonOnly: true,
+                                    buttonCfg: {
+                                        text: '',
+                                        iconCls: 'icon-upload'
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'panel',
+                            hideLabel: true,
+                            layout: 'hbox',
+                            height: 100,
+                            border: false,
+                            items: [
+                                {
+                                    xtype: 'label',
+                                    text: 'Back cover:',
+                                    width: 90
+                                },
+                                {
+                                    xtype: 'component',
+                                    height: 100,
+                                    width: 100,
+                                    autoEl: {
+                                        tag: 'img',
+                                        src: 'http://img.timeinc.net/time/photoessays/2009/top10_airport_books/davinci_code.jpg',
+                                        itemId: 'backCoverPreview'
+                                    }
+                                },
+                                {
+                                    xtype: 'filefield',
+                                    buttonOnly: false,
+                                    buttonText: "Upload",
+                                    name: 'backCoverUpload',
+                                    buttonOnly: true,
+                                    buttonCfg: {
+                                        text: '',
+                                        iconCls: 'icon-upload'
+                                    }
+                                }
+                            ]
                         }
                     ]
                 }
