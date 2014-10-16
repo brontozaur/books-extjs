069135514d02a3117347ab11d18e4749e36a6f16
hypercube21@gmail.com <hypercube21@gmail.com@0d024af5-6d5a-f534-3c1a-20f54fc11ff1>
2014-10-16 19:14:45 +0000

    Proper formatting

--- trunk/webapp/app/controller/BooksGridController.js
+++ trunk/webapp/app/controller/BooksGridController.js
@@ -1,52 +1,59 @@
 Ext.define('BM.controller.BooksGridController', {
-            extend : 'Ext.app.Controller',
-            stores : ['BookStore'],
+            extend: 'Ext.app.Controller',
+            stores: [
+                'BookStore'
+            ],
 
-            model : ['BookModel'],
+            model: [
+                'BookModel'
+            ],
 
-            views : ['book.BooksGrid', 'book.BookWindow'],
+            views: [
+                'book.BooksGrid',
+                'book.BookWindow'
+            ],
 
-            init : function() {
+            init: function() {
                 this.control({
-                            'booksgrid' : {
-                                selectionchange : this.changeselection,
-                                celldblclick : this.celldblclick,
-                                itemkeydown : this.handleKeyPress
+                            'booksgrid': {
+                                selectionchange: this.changeselection,
+                                celldblclick: this.celldblclick,
+                                itemkeydown: this.handleKeyPress
                             },
-                            'booksgrid button[action=add-book]' : {
-                                click : this.addBook
+                            'booksgrid button[action=add-book]': {
+                                click: this.addBook
                             },
-                            'booksgrid button[action=mod-book]' : {
-                                click : this.modBook
+                            'booksgrid button[action=mod-book]': {
+                                click: this.modBook
                             },
-                            'booksgrid button[action=del-book]' : {
-                                click : this.delBook
+                            'booksgrid button[action=del-book]': {
+                                click: this.delBook
                             },
-                            'booksgrid textfield[name=searchField]' : {
-                                change : this.searchBooks
+                            'booksgrid textfield[name=searchField]': {
+                                change: this.searchBooks
                             }
                         });
             },
 
-            changeselection : function(selModel, selected, eOpts) {
+            changeselection: function(selModel, selected, eOpts) {
                 if (selected.length > 0) {
                     enablebuttons(true);
                     this.fillInfoArea(selected[0]);
                 }
             },
 
-            celldblclick : function(grid, td, cellIndex, record, tr, rowIndex, e) {
+            celldblclick: function(grid, td, cellIndex, record, tr, rowIndex, e) {
                 var modButton = Ext.ComponentQuery.query('booksgrid button[action=mod-book]')[0];
                 this.modBook(modButton);
             },
 
-            handleKeyPress : function(grid, record, item, index, event, eOpts) {
+            handleKeyPress: function(grid, record, item, index, event, eOpts) {
                 if (event.keyCode == Ext.EventObject.DELETE && grid.getSelectionModel().hasSelection()) {
                     this.delBook();
                 }
             },
 
-            fillInfoArea : function(record) {
+            fillInfoArea: function(record) {
                 var bookInfo = Ext.ComponentQuery.query('bookinfo')[0];
                 var fieldContainer = bookInfo.down('container[itemId=bookInfoFields]');
 
@@ -119,23 +126,23 @@ Ext.define('BM.controller.BooksGridController', {
                 }
             },
 
-            addBook : function(button, clickEvent, options) {
+            addBook: function(button, clickEvent, options) {
                 var window = Ext.widget('bookwindow');
                 window.show();
             },
 
-            modBook : function(button, clickEvent, options) {
+            modBook: function(button, clickEvent, options) {
                 var delButton = Ext.ComponentQuery.query('booksgrid button[action=del-book]')[0];
                 delButton.disable();
                 var window = Ext.widget('bookwindow');
                 var selectionModel = button.up('viewport').down('booksgrid').getSelectionModel();
                 if (!selectionModel.hasSelection) {
                     Ext.Msg.show({
-                                title : 'Carte neselectata',
-                                msg : 'Selectati o carte',
-                                width : 300,
-                                buttons : Ext.Msg.OK,
-                                icon : Ext.window.MessageBox.WARNING
+                                title: 'Carte neselectata',
+                                msg: 'Selectati o carte',
+                                width: 300,
+                                buttons: Ext.Msg.OK,
+                                icon: Ext.window.MessageBox.WARNING
                             });
                     return;
                 }
@@ -160,58 +167,60 @@ Ext.define('BM.controller.BooksGridController', {
                 window.show();
             },
 
-            delBook : function(button, clickEvent, options) {
+            delBook: function(button, clickEvent, options) {
                 Ext.MessageBox.confirm('Confirmare stergere', 'Sunteti sigur ca doriti sa stergeti cartea selectata?', this.deleteBook);
             },
 
-            deleteBook : function(btn) {
+            deleteBook: function(btn) {
                 if (btn == 'yes') {
                     var booksGrid = Ext.ComponentQuery.query('booksgrid')[0];
                     var selectionModel = booksGrid.getSelectionModel();
                     if (!selectionModel.hasSelection) {
                         Ext.Msg.show({
-                                    title : 'Carte neselectata',
-                                    msg : 'Selectati o carte',
-                                    width : 300,
-                                    buttons : Ext.Msg.OK,
-                                    icon : Ext.window.MessageBox.WARNING
+                                    title: 'Carte neselectata',
+                                    msg: 'Selectati o carte',
+                                    width: 300,
+                                    buttons: Ext.Msg.OK,
+                                    icon: Ext.window.MessageBox.WARNING
                                 });
                         return;
                     }
                     var selectedBook = selectionModel.getSelection()[0];
                     Ext.Ajax.request({
-                                url : 'books',
-                                method : 'POST',
-                                params : {
-                                    event : 'del-book',
-                                    bookId : selectedBook.get('bookId')
+                                url: 'books',
+                                method: 'POST',
+                                params: {
+                                    event: 'del-book',
+                                    bookId: selectedBook.get('bookId')
                                 },
-                                scope : this,
-                                success : function(result, request) {
+                                scope: this,
+                                success: function(result, request) {
                                     clearInfoAreaFields();
                                     enablebuttons(false);
                                     Ext.widget('booksgrid').getStore().load();
                                 },
-                                failure : function(result, request) {
+                                failure: function(result, request) {
                                     createErrorWindow(result);
                                 }
                             });
                 }
             },
 
-            searchBooks : function(textfield, newValue, oldValue) {
+            searchBooks: function(textfield, newValue, oldValue) {
                 var grid = Ext.widget('booksgrid');
                 var store = grid.getStore();
                 store.clearFilter(true);
-                store.filter([{
-                            filterFn : function(record) {
-                                if (Ext.isEmpty(newValue)) {
-                                    return true;
-                                }
-                                var numeAutor = record.get('authorName');
-                                var title = record.get('title');
-                                return numeAutor.indexOf(newValue) > -1 || title.indexOf(newValue) > -1;
+                store.filter([
+                    {
+                        filterFn: function(record) {
+                            if (Ext.isEmpty(newValue)) {
+                                return true;
                             }
-                        }]);
+                            var numeAutor = record.get('authorName');
+                            var title = record.get('title');
+                            return numeAutor.indexOf(newValue) > -1 || title.indexOf(newValue) > -1;
+                        }
+                    }
+                ]);
             }
         });
