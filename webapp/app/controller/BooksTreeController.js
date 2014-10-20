49360f2aeca197a302621e81feb82693150a0692
hypercube21 <hypercube21@gmail.com>
2014-10-20 20:27:27 +0000

    Books grid now shows filtered books on left tree selection.

--- webapp/app/controller/BooksTreeController.js
+++ webapp/app/controller/BooksTreeController.js
@@ -6,5 +6,39 @@ Ext.define('BM.controller.BooksTreeController', {
 
             model: [
                 'BooksTreeModel'
-            ]
+            ],
+
+            init: function() {
+                this.control({
+                            'bookstree': {
+                                beforeload: this.loadParamsToRequest,
+                                itemclick: this.itemClick
+                            }
+                        });
+            },
+
+            loadParamsToRequest: function(store, operation, eOpts) {
+                var node = operation.node;
+                operation.params.nodeId = node.get('name');
+            },
+
+            itemClick: function(tree, recordItem, item, index, e, eOpts) {
+                var treeItemValue = recordItem.get('name');
+                var grid = Ext.widget('booksgrid');
+                var store = grid.getStore();
+                store.clearFilter(true);
+                store.filter([
+                    {
+                        filterFn: function(record) {
+                            if (Ext.isEmpty(treeItemValue)) {
+                                return true;
+                            }
+                            var numeAutor = record.get('authorName');
+                            return numeAutor.indexOf(treeItemValue) > -1;
+                        }
+                    }
+                ]);
+                clearInfoAreaFields();
+                enablebuttons(false);
+            }
         });
