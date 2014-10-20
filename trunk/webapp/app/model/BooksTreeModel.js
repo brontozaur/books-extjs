4c08694b254c8d20361d32dc70f85406c5113259
hypercube21@gmail.com <hypercube21@gmail.com@0d024af5-6d5a-f534-3c1a-20f54fc11ff1>
2014-10-20 21:17:26 +0000

    Small changes for tree.

--- trunk/webapp/app/model/BooksTreeModel.js
+++ trunk/webapp/app/model/BooksTreeModel.js
@@ -9,16 +9,16 @@ Ext.define('BM.model.BooksTreeModel', {
                     name: 'treeItemName',
                     type: 'string',
                     convert: function(newValue, record) {
+                        var howManyBooks = record.get('howManyBooks');
                         if (Ext.isEmpty(record.get('leaf')) || record.get('leaf') === false) {
                             var letter = record.get('name');
                             var howManyAutors = record.get('howManyAutors');
-                            var howManyBooks = record.get('howManyBooks');
                             var autor = howManyAutors === 1 ? 'autor' : 'autori';
                             var carte = howManyBooks === 1 ? 'carte' : 'carti';
 
                             return letter + ' (' + howManyAutors + ' ' + autor + ', ' + howManyBooks + ' ' + carte + ')';
                         }
-                        return record.get('name');
+                        return record.get('name') + ' (' + howManyBooks + ')';
                     }
                 }
             ]
